b52044c550a290474b7250e70d9061f301a19c8d
novassy <mina37@gmail.com>
2019-05-19 17:20:16 +0900

    てｓｔ

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # janken
-janken
+
+
+heroku create
+Creating app... done, ⬢ warm-beach-97622
+https://warm-beach-97622.herokuapp.com/ | https://git.heroku.com/warm-beach-97622.git
--- janken.php
+++ janken.php
@@ -1,74 +1,36 @@
 <?php
-/*
+
+	$hand = ['goo', 'choki', 'pa'];
+	$player = $_GET['player'];
+
+	// $playernum = intval($player);
+	// echo "あなた「".$hand[$playernum]."」\n";
+	//こちらの手の選択および表示
+
+	$rand = array_rand($hand);
+
+	// echo "相手「".$hand[$rand]."！」\n";
+	$comp = intval($rand);
 	$janken_array = [
-		'cpu'=>'choki',
-		'result'=>'win'
+		'cpu'=>$hand[$rand],
+		'result'=>''
 	];
 
-	if ($_GET["player"] == "グー") {
-		echo json_encode($janken_array);
-	} else {
-		$janken_array = [
-			'cpu'=>'other',
-			'result'=>'any'
-		];
-		echo json_encode($janken_array);
+	//グーのとき
+	if ($player == 0 && $comp == 0) {
+		$janken_array['result'] = 'aaa';
 	}
-*/
-
-	//"手を選んでください(0=グー, 1=チョキ, 2=パーを入力)" と表示する
-	//0, 1, 2を入力させる
-	//コンピュータの手をランダムで決める
-	//もし　プレイヤーとコンピュータの手が同じなら
-	//「あいこです」と表示する
-	//もし　プレイヤーが0　でコンピュータが1　なら
-	//「勝ちです」と表示する
-	//もし　プレイヤーが0　でコンピュータが2　なら
-	//「負けです」と表示する
-	//以下なんかそんなかんじ
-	// do {
-		// echo "手を選んでください(0=グー, 1=チョキ, 2=パーを入力)".PHP_EOL;
-		//相手の入力および手の表示
-		
-		$hand = ['goo', 'choki', 'pa'];
-		$player = $_GET['player'];
-
-		// $playernum = intval($player);
-		// echo "あなた「".$hand[$playernum]."」\n";
-		//こちらの手の選択および表示
-		
-		$rand = array_rand($hand);
-		
-		// echo "相手「".$hand[$rand]."！」\n";
-		 $comp = intval($rand);
-		
-		
-		//グーのとき
-		if ($player == 0 && $comp == 0) {
-			$janken_array = [
-				'cpu'=>$hand[$rand],
-				'result'=>'draw'
-			];
-
-			echo json_encode($janken_array);
-		}
-		if ($player == 0 && $comp == 1) {
-			$janken_array = [
-				'cpu'=>$hand[$rand],
-				'result'=>'win'
-			];
-
-			echo json_encode($janken_array);
-		}
-		if ($player == 0 && $comp == 2) {
-			$janken_array = [
-				'cpu'=>$hand[$rand],
-				'result'=>'lose'
-			];
-
-			echo json_encode($janken_array);
-		}
+	if ($player == 0 && $comp == 1) {
+		$janken_array['result'] = 'bbb';
+	}
+	if ($player == 0 && $comp == 2) {
+		$janken_array['result'] = 'ccc';
+	}
 	
+	echo json_encode($janken_array);
+
+
+/*
 		//チョキのとき
 		if ($player == 1 && $comp == 1) {
 		echo "あいこです！もう一度\n".PHP_EOL;
@@ -90,7 +52,7 @@
 		if ($player == 2 && $comp == 1) {
 		echo "あなたの負けです……".PHP_EOL;
 		}
-		
-		
+*/
+
 	// } while ($player == $comp);
 
