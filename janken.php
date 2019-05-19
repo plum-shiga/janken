5c1d1445fed5db12cf13b2ce25182fea09351f6a
novassy <mina37@gmail.com>
2019-05-19 16:12:56 +0900

    プレイヤーがグー時のみ、ジャンケンが成立するようになりました

--- janken.php
+++ janken.php
@@ -1,4 +1,5 @@
 <?php
+/*
 	$janken_array = [
 		'cpu'=>'choki',
 		'result'=>'win'
@@ -13,8 +14,7 @@
 		];
 		echo json_encode($janken_array);
 	}
-
-/*
+*/
 
 	//"手を選んでください(0=グー, 1=チョキ, 2=パーを入力)" と表示する
 	//0, 1, 2を入力させる
@@ -26,29 +26,47 @@
 	//もし　プレイヤーが0　でコンピュータが2　なら
 	//「負けです」と表示する
 	//以下なんかそんなかんじ
-	do {
-		echo "手を選んでください(0=グー, 1=チョキ, 2=パーを入力)".PHP_EOL;
+	// do {
+		// echo "手を選んでください(0=グー, 1=チョキ, 2=パーを入力)".PHP_EOL;
 		//相手の入力および手の表示
-		$hand = ['グー', 'チョキ', 'パー'];
-		if(isset($_GET['player'])){
-			$player = $_GET['player'];
-		}
-		$playernum = intval($player);
-		echo "あなた「".$hand[$playernum]."」\n";
-		//こちらの手の選択および表示
-		$rand = array_rand($hand);
-		echo "相手「".$hand[$rand]."！」\n";
-		$comp = intval($rand);
+		
+		$hand = ['goo', 'choki', 'pa'];
+		$player = $_GET['player'];
 
+		// $playernum = intval($player);
+		// echo "あなた「".$hand[$playernum]."」\n";
+		//こちらの手の選択および表示
+		
+		$rand = array_rand($hand);
+		
+		// echo "相手「".$hand[$rand]."！」\n";
+		 $comp = intval($rand);
+		
+		
 		//グーのとき
 		if ($player == 0 && $comp == 0) {
-		echo "あいこです！もう一度\n".PHP_EOL;
+			$janken_array = [
+				'cpu'=>$hand[$rand],
+				'result'=>'draw'
+			];
+
+			echo json_encode($janken_array);
 		}
 		if ($player == 0 && $comp == 1) {
-		echo "あなたの勝ちです！".PHP_EOL;
+			$janken_array = [
+				'cpu'=>$hand[$rand],
+				'result'=>'win'
+			];
+
+			echo json_encode($janken_array);
 		}
 		if ($player == 0 && $comp == 2) {
-		echo "あなたの負けです……".PHP_EOL;
+			$janken_array = [
+				'cpu'=>$hand[$rand],
+				'result'=>'lose'
+			];
+
+			echo json_encode($janken_array);
 		}
 	
 		//チョキのとき
@@ -72,5 +90,7 @@
 		if ($player == 2 && $comp == 1) {
 		echo "あなたの負けです……".PHP_EOL;
 		}
-	} while ($player == $comp);
-*/
+		
+		
+	// } while ($player == $comp);
+
